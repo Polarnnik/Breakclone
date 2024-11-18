8061ca77066b4d915f507dab74abfe8fa8e46b12
Polarnichek <rostislav.voloshin@yandex.ru>
2024-11-19 02:57:31 +0700

    Its playable now

--- States/play_state.cpp
+++ States/play_state.cpp
@@ -4,6 +4,7 @@
 
 #include "play_state.h"
 #include "../window.h"
+#include "main_screen.h"
 #include <raylib.h>
 
 PlayState::PlayState(App* app) : m_app(app), ball(app) {
@@ -22,19 +23,39 @@ void PlayState::render() {
 
 void PlayState::handleInput() {
     if (IsKeyDown(KEY_A)) {
-        paddle.x -= 0.2;
+        if (paddle.x > 0) {
+            paddle.x -= 0.2;
+        }
     }
     if (IsKeyDown(KEY_D)) {
+        if (paddle.x + paddle.width < Window::getWidth()) {
             paddle.x += 0.2;
+        }
     }
 }
 
 void PlayState::logic() {
     ball.move();
+    checkCollisions();
 }
 
 void PlayState::checkCollisions() {
+    if (CheckCollisionCircleRec(ball.getPosition(), ball.getRadius(), paddle)) {
+        ball.reflectVelocity();
+    }
 
+    for (auto it = brics.begin(); it != brics.end(); ) {
+        if (CheckCollisionCircleRec(ball.getPosition(), ball.getRadius(), *it)) {
+            ball.reflectVelocity();
+
+            it = brics.erase(it);
+            if (brics.size() == 0) {
+                m_app->changeState(std::make_unique<MainScreen>(m_app));
+            }
+        } else {
+            ++it;
+        }
+    }
 }
 
 void PlayState::initGame() {
